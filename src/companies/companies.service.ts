9c65b29d53f1d1c6c0133ff5b5f788bd0d6ae1e6
richardbmezzomo <richardmezzomo1@gmail.com>
2025-01-01 09:13:02 -0300

    feat: add company controllers

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -2,17 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Company } from './company.entity';
 import { Repository } from 'typeorm';
+import { CreateCompanyDto } from './create-company.dto';
+import { Contact } from 'src/contacts/contact.entity';
+import { UpdateCompanyDto } from './update-company.dto';
 
 @Injectable()
 export class CompaniesService {
   constructor(
     @InjectRepository(Company)
     private readonly companyRepository: Repository<Company>,
+
+    @InjectRepository(Contact) // Injetar o repositório de Contact
+    private readonly contactRepository: Repository<Contact>,
   ) {}
 
-  async create(data: Partial<Company>): Promise<Company> {
-    const company = this.companyRepository.create(data);
-    return this.companyRepository.save(company);
+  async create(data: CreateCompanyDto): Promise<Company> {
+    const company = this.companyRepository.create({ title: data.title });
+    const savedCompany = await this.companyRepository.save(company);
+
+    if (data.contacts && data.contacts.length > 0) {
+      const contacts = data.contacts.map((contact) =>
+        this.contactRepository.create({ ...contact, company: savedCompany }),
+      );
+
+      await this.contactRepository.save(contacts);
+    }
+    return this.companyRepository.findOne({
+      where: { id: savedCompany.id },
+      relations: ['contacts'],
+    });
   }
 
   async findAll(): Promise<Company[]> {
@@ -26,12 +44,66 @@ export class CompaniesService {
     });
   }
 
-  async update(id: number, data: Partial<Company>): Promise<Company> {
-    await this.companyRepository.update(id, data);
-    return this.findOne(id);
+  async update(id: number, data: UpdateCompanyDto): Promise<Company> {
+    const company = await this.companyRepository.findOne({
+      where: { id },
+      relations: ['contacts'],
+    });
+
+    if (!company) {
+      throw new Error(`Empresa não encontrada!`);
+    }
+
+    // Atualiza o título da empresa
+    if (data.title) {
+      company.title = data.title;
+    }
+
+    if (data.contacts) {
+      const updatedContacts = [];
+
+      for (const contact of data.contacts) {
+        if (contact.id) {
+          const existingContact = await this.contactRepository.findOne({
+            where: { id: contact.id, company: { id } },
+          });
+
+          if (existingContact) {
+            Object.assign(existingContact, contact);
+            updatedContacts.push(existingContact);
+          }
+        } else {
+          const newContact = this.contactRepository.create({
+            ...contact,
+            company,
+          });
+          updatedContacts.push(newContact);
+        }
+      }
+
+      await this.contactRepository.save(updatedContacts);
+
+      company.contacts = updatedContacts;
+    }
+
+    await this.companyRepository.save(company);
+
+    return this.companyRepository.findOne({
+      where: { id },
+      relations: ['contacts'],
+    });
   }
 
   async remove(id: number): Promise<void> {
-    await this.companyRepository.delete(id);
+    const company = await this.companyRepository.findOne({
+      where: { id },
+      relations: ['contacts'],
+    });
+
+    if (!company) {
+      throw new Error(`Empresa não encontrada!`);
+    }
+
+    await this.companyRepository.remove(company);
   }
 }
--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contact } from './contact.entity';
+import { CompaniesModule } from 'src/companies/companies.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contact])],
+  imports: [TypeOrmModule.forFeature([Contact]), CompaniesModule],
   providers: [ContactsService],
 })
 export class ContactsModule {}
