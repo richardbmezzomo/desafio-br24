cc7e55f3b6df4e4743da6a7c11d3ec202ea2cbd8
richardbmezzomo <richardmezzomo1@gmail.com>
2025-01-13 15:41:38 +0000

    fix: CORS error

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://www.richardbmezzomo.site/',
+    origin: 'https://www.richardbmezzomo.site',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
