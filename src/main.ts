f3b51316ecaa24fd1725b4cd282017b2bbea89d7
richardbmezzomo <richardmezzomo1@gmail.com>
2025-02-05 12:25:27 -0300

    feat: add CORS to all

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://www.richardbmezzomo.site',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
   });
